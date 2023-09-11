a94f678ccef331640c70b1d4a751b2f74bd569f6
Mariana Vieira <marianavca002@outlook.com>
2023-09-10 23:44:43 -0300

    Implementing doubly linked list

---
+++ src/DoubleList.java
@@ -0,0 +1,155 @@
+public class DoubleList <T> {
+    Node<T> first;
+    Node<T> last;
+
+    int count;
+
+    public DoubleList() {
+        first = null;
+        last = null;
+    }
+
+    public void addAtBeginning(T element) {
+        Node<T> newElement = new Node<>(element);
+
+        if (first == null) {
+            first = newElement;
+            last = newElement;
+        } else {
+            newElement.next = first;
+            first.previous = newElement;
+            first = newElement;
+        }
+        count++;
+    }
+
+    public void add(T element) {
+        Node<T> newElement = new Node<>(element);
+
+        if (first == null) {
+            first = newElement;
+            last = newElement;
+        } else {
+            last.next = newElement;
+            newElement.previous = last;
+            last = newElement;
+        }
+        count++;
+    }
+
+    public void add(int position, T element) {
+        if (position == 0) {
+            addAtBeginning(element);
+        } else if (position == size() - 1) {
+            add(element);
+        } else {
+            Node<T> aux = first;
+            Node<T> newElement = new Node<>(element);
+            for (int i = 0; i < position; i++) {
+                aux = aux.next;
+            }
+            aux.previous.next = newElement;
+            newElement.previous = aux.previous;
+            newElement.next = aux;
+            aux.previous = newElement;
+        }
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public void clear() {
+        first = null;
+        count = 0;
+    }
+
+    public boolean contains(T element) {
+        Node<T> aux = first;
+
+        for (int i = 0; i < count - 1; i++) {
+            if (aux.data.equals(element))
+                return true;
+            aux = aux.next;
+        }
+        return false;
+    }
+
+    public void remove(T element) {
+        Node<T> aux = first;
+
+        while (aux != null) {
+            if (aux.data.equals(element)) {
+                if (aux.equals(first)) {
+                    first = aux.next;
+                    aux = first;
+                } else if (aux.equals(last)) {
+                    last.previous.next = null;
+                    last = last.previous;
+                    aux = null;
+                } else {
+                    aux.previous.next = aux.next;
+                    aux.next.previous = aux.previous;
+                    aux = aux.next;
+                }
+                count--;
+            } else {
+                aux = aux.next;
+            }
+        }
+    }
+
+    public void removeAt(int position) {
+        if (position == size() - 1) {
+            last.previous.next = null;
+            last = last.previous;
+        } else if (position == 0) {
+            first.next.previous = null;
+            first = first.next;
+        } else {
+            Node<T> aux = first;
+
+            for (int i = 0; i < position; i++) {
+                aux = aux.next;
+            }
+
+            aux.previous.next = aux.next;
+            aux.next.previous = aux.previous;
+        }
+    }
+
+    public Object get(int index) {
+        if (index > count)
+            return -1;
+
+        Node<T> aux = first;
+        for (int i = 0; i < index; i++) {
+            aux = aux.next;
+        }
+        return aux.data;
+    }
+
+    public int indexOf(Object element) {
+        Node<T> aux = first;
+
+        for (int i = 0; i < count; i++) {
+            if (aux.data.equals(element))
+                return i;
+            aux = aux.next;
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        String str = "[";
+        Node<T> aux = first;
+
+        while (aux != null) {
+            str += " " + aux.data;
+            aux = aux.next;
+        }
+        return str + " ]";
+    }
+
+}
--- src/Node.java
+++ src/Node.java
@@ -1,9 +1,11 @@
 public class Node <T> {
     public T data;
     public Node<T> next;
+    public Node<T> previous;
 
     public Node(T dado) {
         this.data = dado;
+        previous = null;
         next = null;
     }
 }
